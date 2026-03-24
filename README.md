c810162f1fa08ec0e39724d963045867e7ca5c30
favoursui <samuelfavour858@gmail.com>
2026-03-24 13:07:02 +0100

    color import for background and text

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 npm run tailwind --this helps you execute your tailwind 
 npm run prettier --help you run prettier to reformat your lines of code
+
+bg-cream-dark --cream color for the background
+text-brown --brown color for text
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: {
+          DEFAULT: "#FDF6EC",
+          dark:    "#F5E9D4",
+          mid:     "#EDD9B8",
+        },
+        brown: {
+          DEFAULT: "#3C1F0A",
+          mid:     "#6B3A1F",
+          light:   "#9B6B47",
+        },
+        gold: {
+          DEFAULT: "#C49A3C",
+          light:   "#E2C27D",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
 
